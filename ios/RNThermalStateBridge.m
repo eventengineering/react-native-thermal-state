59189d8d730198a7a995d421eb01a4b685e6a182
Liam Le Brun <liam@event.engineering>
2019-07-26 14:40:00 +0100

    [Swift] Fully switch to swift

---
+++ ios/RNThermalStateBridge.m
@@ -0,0 +1,14 @@
+//
+//  RNThermalStateBridge.m
+//  RNThermalState
+//
+//  Created by Event on 26/07/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(RNThermalState, NSObject)
+
+@end
